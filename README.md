a226837968dd95e3ec27e05edf9a0abf4f9af3bd
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-04-01 00:06:21 -0400

    stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+# elfos-php-forum
+
+A simple forum written in PHP.
+
+Back again with another php project. Needed a refresher on PHP since I haven't been doing much work lately (Sorry BRICKTown!).
--- layout.php
+++ layout.php
@@ -6,16 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $name ?> - Elfo's Forum</title>
     <link rel="stylesheet" href="/css/style.css">
+
 </head>
 
 <body>
     <div class="container">
-        <?php
-        // include view
-        include($view);
-        ?>
+        <!-- header -->
+        <div class="wrapper">
+            <div class="content">
+                <div class="brand">
+                    <h1 class="center">
+                        <?php echo $name ?> - Elfo's Forum
+                    </h1>
+
+                </div>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="content">
+                <div class="links">
+                    <a href="/forum">forum</a>
+                    <a href="/forum">register</a>
+                    <a href="/forum">login</a>
+                </div>
+            </div>
+        </div>
+        <!-- page content -->
+        <div class="wrapper">
+            <div class="content">
+                <?php
+                // include view
+                include($view);
+                ?>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="content">
+                Elfo's Forum - PHP Project
+                &bull;
+                <a href="https://github.com/eIfo1/elfos-php-forum">github repo</a>
+                &bull;
+                <a href="https://github.com/eIfo1/elfos-php-forum/issues">issues</a>
+                &bull;
+                <a href="https://github.com/eIfo1/elfos-php-forum/blob/master/LICENSE">license</a>
+                &bull;
+                <a href="https://github.com/eIfo1/">github profile</a>
+            </div>
+        </div>
     </div>
-    <hr>
 
 </body>
 
